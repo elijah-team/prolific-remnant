7ef88ecc6c7d3db8c87edf3c40c51ad6e6e3171a
Tripleo Nova Szi <szu@tripleo.milkyway>
2023-11-19 03:23:03 -0500

    Move and Delete

--- src/main/java/tripleo/elijah/util/SimplePrintLoggerToRemoveSoon.java
+++ src/main/java/tripleo/elijah/util/SimplePrintLoggerToRemoveSoon.java
@@ -9,11 +9,15 @@ public class SimplePrintLoggerToRemoveSoon {
 //		System.out.println(aS);
 	}
 
-	public static void println2(final Object aS) {
+	public static void println2(final String aS) {
 //		System.out.println(""+aS);
 	}
 
-	public static void println_err2(final Object aS) {
+	public static void println_err2(final String aS) {
+//		System.err.println(""+aS);
+	}
+
+	public static void println_out_2(final String aS) {
 //		System.err.println(""+aS);
 	}
 }
