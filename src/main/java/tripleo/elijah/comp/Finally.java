4e2f5a0cc526f66a68250a20f5a42f7f39eb61a9
Tripleo Nova <tripleo@tripleo.milkyway>
2023-11-24 12:20:52 -0500

    refactor: Move EOT_FileNameProvider to top level
    - imports

--- src/main/java/tripleo/elijah/comp/Finally.java
+++ src/main/java/tripleo/elijah/comp/Finally.java
@@ -1,9 +1,13 @@
 package tripleo.elijah.comp;
 
-import org.jetbrains.annotations.*;
-import tripleo.elijah.nextgen.outputtree.*;
+import org.jetbrains.annotations.NotNull;
+import tripleo.elijah.nextgen.outputtree.EOT_FileNameProvider;
+import tripleo.elijah.nextgen.outputtree.EOT_OutputFile;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Finally {
 	private final Set<Outs>    outputOffs = new HashSet<>();
@@ -19,13 +23,9 @@ public class Finally {
 		return !turnAllOutputOff && !outputOffs.contains(aOuts);
 	}
 
-	public void addInput(final Nameable aNameable, final Out2 ty) {
-		inputs.add(new Input(aNameable, ty));
+	public void addInput(final CompilerInput aInp, final Out2 ty) {
+		inputs.add(new Input(aInp, ty));
 	}
-
-//	public void addInput(final CompilerInput aInp, final Out2 ty) {
-//		inputs.add(new Input(aInp, ty));
-//	}
 
 	public boolean containsInput(final String aS) {
 		return inputs.stream().anyMatch(i -> i.name().equals(aS));
@@ -35,19 +35,31 @@ public class Finally {
 		turnAllOutputOff = true;
 	}
 
-//	public void addInput(final CompFactory.InputRequest aInp, final Out2 ty) {
-//		inputs.add(new Input(aInp, ty));
-//	}
+	public void addInput(final CompFactory.InputRequest aInp, final Out2 ty) {
+		inputs.add(new Input(aInp, ty));
+	}
 
 	public boolean containsCodeOutput(@NotNull final String s) {
 		return outputs.stream().anyMatch(i -> i.name().equals(s));
 	}
 
-	public void addCodeOutput(final EOT_OutputFile.FileNameProvider aFileNameProvider, final EOT_OutputFile aOff) {
+	public void addCodeOutput(final EOT_FileNameProvider aFileNameProvider, final EOT_OutputFile aOff) {
 		outputs.add(new Output(aFileNameProvider, aOff));
 	}
 
-	public enum Outs {Out_6262, Out_727, Out_350, Out_364, Out_252, Out_2121, Out_486, Out_5757, Out_1069, Out_141, Out_EVTE_159, Out_401b}
+	public int codeOutputSize() {
+		return outputs.size();
+	}
+
+	public int codeInputSize() {
+		return inputs.size();
+	}
+
+	public boolean containsCodeInput(final String aS) {
+		return containsInput(aS);
+	}
+
+	public enum Outs {Out_6262, Out_727, Out_350, Out_364, Out_252, Out_2121, Out_486, Out_5757, Out_1069, Out_141, Out_EVTE_159, Out_353, Out_120, Out_40, Out_401b}
 
 	public enum Out2 {
 		EZ, ELIJAH
@@ -61,11 +73,6 @@ public class Finally {
 
 		private final Nameable nameable;
 		private final Out2 ty;
-
-		public Input(final Nameable aNameable, final Out2 aTy) {
-			nameable = aNameable;
-			ty       = aTy;
-		}
 
 		public Input(final CompilerInput aInp, final Out2 aTy) {
 			nameable = new Nameable() {
@@ -87,30 +94,37 @@ public class Finally {
 			ty = aTy;
 		}
 
+		public String name() {
+			return nameable.getName();
+		}
+
 		@Override
 		public String toString() {
 			return "Input{" +
-			  "name=" + nameable.getName() +
-			  ", ty=" + ty +
-			  '}';
-		}
-
-		public String name() {
-			return nameable.getName();
+					"nameable=" + nameable.getName().toString() +
+					", ty=" + ty +
+					'}';
 		}
 	}
 
 	class Output {
-		private final EOT_OutputFile.FileNameProvider fileNameProvider;
+		private final EOT_FileNameProvider fileNameProvider;
 		private final EOT_OutputFile off;
 
-		public Output(final EOT_OutputFile.FileNameProvider aFileNameProvider, final EOT_OutputFile aOff) {
+		public Output(final EOT_FileNameProvider aFileNameProvider, final EOT_OutputFile aOff) {
 			fileNameProvider = aFileNameProvider;
 			off              = aOff;
 		}
 
 		public String name() {
 			return fileNameProvider.getFilename();
+		}
+
+		@Override
+		public String toString() {
+			return "Output{" +
+					"fileNameProvider=" + fileNameProvider.getFilename().toString() +
+					'}';
 		}
 	}
 }
