d5fa16a7f5268e6e265630d5b6b78c9691ad27b1
Tripleo Nova <tripleo@tripleo.milkyway>
2023-11-24 07:46:24 -0500

    [comp/ci] CM_Filename and LocatableString

--- src/main/java/tripleo/elijah/ci/CompilerInstructionsImpl.java
+++ src/main/java/tripleo/elijah/ci/CompilerInstructionsImpl.java
@@ -14,22 +14,21 @@ import com.google.common.collect.Collections2;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.jetbrains.annotations.NotNull;
 import tripleo.elijah.ci.i.CompilerInstructions;
+import tripleo.elijah.compiler_model.CM_Filename;
 import tripleo.elijah.lang.i.IExpression;
 import tripleo.elijah.lang.i.StringExpression;
 import tripleo.elijah.util.Helpers;
+import tripleo.elijah.xlang.LocatableString;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created 9/6/20 11:20 AM
  */
 public class CompilerInstructionsImpl implements CompilerInstructions {
-	public @NotNull List<LibraryStatementPart> lsps = new ArrayList<LibraryStatementPart>();
+	public @NotNull List<LibraryStatementPart> lsps = new ArrayList<>();
 	private         CiIndexingStatement        _idx;
-	private         String                     filename;
+	private CM_Filename filename;
 	private         GenerateStatement          gen;
 	private         String                     name;
 
@@ -46,27 +45,25 @@ public class CompilerInstructionsImpl implements CompilerInstructions {
 	}
 
 	@Override
-	@Nullable
-	public String genLang() {
-		Collection<GenerateStatementImpl.Directive> gens = Collections2.filter(((GenerateStatementImpl) gen).dirs, new Predicate<GenerateStatementImpl.Directive>() {
-			@Override
-			public boolean apply(GenerateStatementImpl.@Nullable Directive input) {
-				assert input != null;
-				if (input.getName().equals("gen")) {
-					return true;
-				}
-				return false;
-			}
-		});
+	public List<LibraryStatementPart> getLibraryStatementParts() {
+		return lsps;
+	}
+
+	@Override
+	public Optional<String> genLang() {
+		Collection<GenerateStatementImpl.Directive> gens = Collections2.filter(((GenerateStatementImpl) gen).dirs, (GenerateStatementImpl.Directive directive) -> {
+            return directive.sameName("gen");
+        });
 		Iterator<GenerateStatementImpl.Directive> gi = gens.iterator();
 		if (!gi.hasNext()) return null;
 		IExpression lang_raw = gi.next().getExpression();
 		assert lang_raw instanceof StringExpression;
-		return Helpers.remove_single_quotes_from_string(((StringExpression) lang_raw).getText());
+		String s = Helpers.remove_single_quotes_from_string(((StringExpression) lang_raw).getText());
+		return Optional.of(s);
 	}
 
 	@Override
-	public String getFilename() {
+	public CM_Filename getFilename() {
 		return filename;
 	}
 
@@ -76,7 +73,12 @@ public class CompilerInstructionsImpl implements CompilerInstructions {
 	}
 
 	@Override
-	public void setFilename(final String filename) {
+	public LocatableString getLocatableName() {
+		return null;
+	}
+
+	@Override
+	public void setFilename(final CM_Filename filename) {
 		this.filename = filename;
 	}
 
@@ -89,19 +91,19 @@ public class CompilerInstructionsImpl implements CompilerInstructions {
 	}
 
 	@Override
-	public void setName(String name) {
-		this.name = name;
+	public void setName(LocatableString name) {
+
 	}
 
-	@Override
-	public void setName(@NotNull Token name) {
-		this.name = name.getText();
-	}
-
-	@Override
-	public List<LibraryStatementPart> lsps() {
-		return lsps;
-	}
+//	@Override
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	@Override
+//	public void setName(@NotNull Token name) {
+//		this.name = name.getText();
+//	}
 
 	@Override
 	public String toString() {
---
+++ src/main/java/tripleo/elijah/compiler_model/CM_Filename.java
@@ -0,0 +1,16 @@
+package tripleo.elijah.compiler_model;
+
+import java.io.File;
+
+public interface CM_Filename {
+    static CM_Filename fromFileToString(File file) {
+        return new CM_Filename() {
+            @Override
+            public String getString() {
+                return file.toString();
+            }
+        };
+    }
+
+    String getString();
+}
