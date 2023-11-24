3ebaa58c462fcf602aeac218ed30831a03d73b80
Tripleo Nova <tripleo@tripleo.milkyway>
2023-11-24 13:18:28 -0500

    f: use events

--- src/main/java/tripleo/elijah/comp/internal/CW_inputIsDirectory.java
+++ src/main/java/tripleo/elijah/comp/internal/CW_inputIsDirectory.java
@@ -12,6 +12,7 @@ import tripleo.elijah.comp.i.ILazyCompilerInstructions;
 import tripleo.elijah.comp.percy.CN_CompilerInputWatcher;
 import tripleo.elijah.comp.queries.QuerySearchEzFiles;
 import tripleo.elijah.diagnostic.Diagnostic;
+import tripleo.elijah.nextgen.comp_model.CM_CompilerInput;
 import tripleo.elijah.util.Maybe;
 import tripleo.elijah.util.Operation2;
 
@@ -23,8 +24,8 @@ import static tripleo.elijah.util.Helpers.List_of;
 
 public class CW_inputIsDirectory {
 	public static void apply(final @NotNull CompilerInput input,
-							 final @NotNull Compilation c,
-							 final @NotNull File f) {
+	                         final CompilationClosure c,
+	                         final @NotNull File f) {
 		CompilerInstructions ez_file;
 
 		final CM_CompilerInput cm = c.getCompilation().get(input);
@@ -32,49 +33,52 @@ public class CW_inputIsDirectory {
 
 		final List<CompilerInstructions> ezs = searchEzFiles(f, c);
 
-        switch (ezs.size()) {
-            case 0 -> {
-                final Diagnostic d_toomany = new TooManyEz_ActuallyNone();
-                final Maybe<ILazyCompilerInstructions> m = new Maybe<>(null, d_toomany);
-                c.compilerInputWatcher_Event(CN_CompilerInputWatcher.e.ACCEPT_CI, input, null);
-                input.accept_ci(m); // TDOO 11/24 move this??
-            }
-            case 1 -> {
-                ez_file = ezs.get(0);
-                final ILazyCompilerInstructions ilci = ILazyCompilerInstructions.of(ez_file);
-                final Maybe<ILazyCompilerInstructions> m3 = new Maybe<>(ilci, null);
-                c.compilerInputWatcher_Event(CN_CompilerInputWatcher.e.ACCEPT_CI, input, null);
-                input.accept_ci(m3); // TDOO 11/24 move this??
-            }
-            default -> {
-                //final Diagnostic d_toomany = new TooManyEz_UseFirst();
-                //add_ci(ezs.get(0));
+		switch (ezs.size()) {
+			case 0 -> {
+				final Diagnostic d_toomany = new TooManyEz_ActuallyNone();
+				final Maybe<ILazyCompilerInstructions> m = new Maybe<>(null, d_toomany);
+				c.compilerInputWatcher_Event(CN_CompilerInputWatcher.e.ACCEPT_CI, input, m);
+				input.accept_ci(m); // TDOO 11/24 move this??
+				assert false;
+			}
+			case 1 -> {
+				ez_file = ezs.get(0);
+				final ILazyCompilerInstructions ilci = ILazyCompilerInstructions.of(ez_file);
+				final Maybe<ILazyCompilerInstructions> m3 = new Maybe<>(ilci, null);
+				c.compilerInputWatcher_Event(CN_CompilerInputWatcher.e.ACCEPT_CI, input, m3);
+				input.accept_ci(m3); // TDOO 11/24 move this??
+				assert false;
+			}
+			default -> {
+				//final Diagnostic d_toomany = new TooManyEz_UseFirst();
+				//add_ci(ezs.get(0));
 
-                // more than 1 (negative is not possible)
-                final Diagnostic d_toomany2 = new TooManyEz_BeSpecific();
-                final Maybe<ILazyCompilerInstructions> m2 = new Maybe<>(null, d_toomany2);
-                c.compilerInputWatcher_Event(CN_CompilerInputWatcher.e.ACCEPT_CI, input, null);
-                input.accept_ci(m2); // TDOO 11/24 move this??
-            }
-        }
+				// more than 1 (negative is not possible)
+				final Diagnostic d_toomany2 = new TooManyEz_BeSpecific();
+				final Maybe<ILazyCompilerInstructions> m2 = new Maybe<>(null, d_toomany2);
+				c.compilerInputWatcher_Event(CN_CompilerInputWatcher.e.ACCEPT_CI, input, m2);
+				input.accept_ci(m2); // TDOO 11/24 move this??
+				assert false;
+			}
+		}
 
 		final Finally.Input input2 = cm.createInput(Finally.Out2.EZ);
-		c.reports().addInput(input2);
+		c.getCompilation().reports().addInput(input2);
 	}
 
 	private static List<CompilerInstructions> searchEzFiles(final @NotNull File directory, final @NotNull CompilationClosure ccl) {
-		final QuerySearchEzFiles                     q    = new QuerySearchEzFiles(ccl);
+		final QuerySearchEzFiles q = new QuerySearchEzFiles(ccl);
 		final Operation2<List<CompilerInstructions>> olci = q.process(directory);
 
 		switch (olci.mode()) {
-		case SUCCESS -> {
-			return olci.success();
-		}
-		case FAILURE -> {
-			ccl.errSink().reportDiagnostic(olci.failure());
-			return List_of();
-		}
-		default -> throw new IllegalStateException("Unexpected value: " + olci.mode());
+			case SUCCESS -> {
+				return olci.success();
+			}
+			case FAILURE -> {
+				ccl.errSink().reportDiagnostic(olci.failure());
+				return List_of();
+			}
+			default -> throw new IllegalStateException("Unexpected value: " + olci.mode());
 		}
 	}
 }
