96820426d9eda00c5b5fa8164c943ff6cc53bb21
Tripleo <Tripleo Nova tripleo@Jimothy2.tripleo.tld>
2024-02-14 03:34:48 -0500

    5: {+CRT} Less noise

--- src/main/java/tripleo/elijah_prolific/v/V.java
+++ src/main/java/tripleo/elijah_prolific/v/V.java
@@ -1,13 +1,19 @@
 package tripleo.elijah_prolific.v;
 
-import tripleo.elijah.stages.gen_generic.GenerateResult;
-import tripleo.elijah.stages.gen_generic.GenerateResultItem;
+import tripleo.elijah.stages.gen_generic.*;
 
 import java.io.PrintStream;
 
 public class V {
 	public static void asv(final e aE, final String aKey) {
-		System.err.println("{{V.asv}} " + aE + " " + aKey);
+		switch (aE) {
+		case TEST_TB_FACT1_168, TEST_TB_FACT1_173 -> {
+			return;
+		}
+		default -> {
+			System.err.println("{{V.asv}} " + aE + " " + aKey);
+		}
+		}
 	}
 
 	public static void gri(final GenerateResult gr) {
@@ -23,7 +29,7 @@ public class V {
 //			stream.println(ns);
 			final String bt = "" + ab.buffer.getText();
 //			stream.println(bt);
-			System.err.println("{{V.gr}} "+ty+" "+ou+" "+ns);
+			System.err.println("{{V.gr}} " + ty + " " + ou + " " + ns);
 		}
 	}
 
@@ -31,5 +37,5 @@ public class V {
 		System.err.println("{{V.exit}}");
 	}
 
-	public enum e {f202_writing_logs, _putSeq, DT2_1785, d399_147, TEST_TB_FACT1_168, TEST_TB_FACT1_173, DT2_2304}
+	public enum e {f202_writing_logs, _putSeq, DT2_1785, d399_147, TEST_TB_FACT1_168, TEST_TB_FACT1_173, CB_INST_CI, USE_MOD_ABSOLUTE_PATH, DT2_2304}
 }
